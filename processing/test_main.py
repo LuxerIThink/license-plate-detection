ffe5469f2628bf1be767dd128ea81b825f5c284f
LuxerIThink <luxerthegamer@gmail.com>
2023-05-25 11:19:08 +0200

    Format code

--- processing/test_main.py
+++ processing/test_main.py
@@ -5,20 +5,23 @@ import time
 import os
 from pathlib import Path
 
+
 @pytest.fixture
 def run_main():
     def run_main_with_args(main_path, img_path, output_file):
-        command = ['python3', main_path, img_path, output_file]
+        command = ["python3", main_path, img_path, output_file]
         result = check_output(command)
-        return result.decode('utf-8')
+        return result.decode("utf-8")
+
     return run_main_with_args
+
 
 def calculate_correct_chars(expected_output, actual_output):
     correct_chars = 0
     total_chars = 0
 
     for key, expected_value in expected_output.items():
-        actual_value = actual_output.get(key, '')
+        actual_value = actual_output.get(key, "")
         for expected_char, actual_char in zip(expected_value, actual_value):
             if expected_char == actual_char:
                 correct_chars += 1
@@ -27,12 +30,13 @@ def calculate_correct_chars(expected_output, actual_output):
     percentage_correct = (correct_chars / total_chars) * 100
     return percentage_correct
 
+
 def test_main(run_main):
     file_path = Path(__file__).resolve().parent
     img_path = file_path.parent / "test_data"
     output_file = file_path.parent / "test_output.txt"
     solution_file = file_path / "test_solution.txt"
-    main_path = file_path.parent / 'Pawlowski_Adam.py'
+    main_path = file_path.parent / "Pawlowski_Adam.py"
 
     with open(solution_file, "r") as f:
         expected_output = json.load(f)
@@ -59,4 +63,3 @@ def test_main(run_main):
     print("Percentage of Correct Characters:", percentage_correct)
 
     assert percentage_correct >= 80
-
